efebd06fbe710153c232507470bcae506e7c48ec
Tianhua Han <tianhua_han@yahoo.com>
2024-02-24 16:48:53 +0000

    update

--- examples/03_maven_opencsv/maven_opencsv/src/test/java/tian/example/AppTest.java
+++ examples/03_maven_opencsv/maven_opencsv/src/test/java/tian/example/AppTest.java
@@ -12,9 +12,39 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
+    // @Test
+    // public void shouldAnswerWithTrue()
+    // {
+    //     assertTrue(true);
+    // }
+    
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testApp() {
+        // App app = new App();
+        // assertTrue(app instanceof App);
+
+        Boolean thrown = false;
+        try {
+            App.main(new String[] {"src/main/resources/example.csv"});
+        } catch (Exception e) {
+            e.printStackTrace();
+            thrown = true;
+        }
+        assertTrue(!thrown);
+    }
+
+    @Test
+    public void testApp_missing_file() {
+        // App app = new App();
+        // assertTrue(app instanceof App);
+
+        Boolean thrown = false;
+        try {
+            App.main(new String[] {"src/main/resources/junk.csv"});
+        } catch (Exception e) {
+            e.printStackTrace();
+            thrown = true;
+        }
+        assertTrue(thrown);
     }
 }
